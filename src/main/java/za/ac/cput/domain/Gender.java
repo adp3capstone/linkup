60541a88d5d99b56682cd6fb11c4fb050f86a302
miss-puse <sejahope63@gmail.com>
2025-05-11 21:07:20 +0200

    Added my details (#18)

---
+++ src/main/java/za/ac/cput/domain/Gender.java
@@ -0,0 +1,12 @@
+package za.ac.cput.domain;
+/*Gender.java
+Author: SH Kgomokaboya(222152672)
+Date : 11May 2025
+ */
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER,
+    PREFER_NOT_TO_SAY
+}
