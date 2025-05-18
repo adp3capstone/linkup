c75dfc2c5d1ebc39e0842b7b3e4d4d948ed54eb9
EthanLeRoux <127761539+EthanLeRoux@users.noreply.github.com>
2025-05-18 16:53:07 +0200

    Add utility class Helper with common validation methods
    Introduced the Helper class to centralize basic validation logic, such as null or empty checks for Strings, integers, doubles, and objects. This improves code reusability and reduces redundancy across the project.

---
+++ src/main/java/za/ac/cput/adp3capstone/linkup/util/Helper.java
@@ -0,0 +1,40 @@
+package za.ac.cput.adp3capstone.linkup.util;
+
+public class Helper {
+    public static boolean isStringNullOrEmpty(String x){
+        if(x.isEmpty() || x == null)
+            return true;
+        return false;
+    }
+
+    public static boolean isIntNull(int y){
+        if(y == 0)
+            return true;
+        return false;
+    }
+
+    public static boolean isIntNegative(int y){
+        if(y < 0)
+            return true;
+        return false;
+    }
+
+    public static boolean isIntZero(int y){
+        if(y == 0)
+            return true;
+        return false;
+    }
+
+    public static boolean isDoubleNull(double z){
+        if(z == 0.0)
+            return true;
+        return false;
+    }
+
+    public static boolean isObjectNull(Object object){
+        if (object == null){
+            return true;
+        }
+        return false;
+    }
+}
