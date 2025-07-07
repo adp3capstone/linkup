0369293dab78264c52772e2c984f40b062619fa8
Brezano Liebenberg <165208156+Zano187@users.noreply.github.com>
2025-07-07 19:55:20 +0200

    Created MessageRepository & MessageRepositoryTest classes (#56)
    * Updated enums & added author details
    
    * Updated Enums
    
    * Updated Enums
    
    * Added MessageFactory & MessageFactoryTest, Modified Helper
    
    * Create MessageRepository Interface & MessageRepositoryTest, added application.properties, updated pom.xml, Message class & Main class

--- src/main/java/za/ac/cput/adp3capstone/linkup/Main.java
+++ src/main/java/za/ac/cput/adp3capstone/linkup/Main.java
@@ -1,7 +1,13 @@
 package za.ac.cput.adp3capstone.linkup;
 
-public class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SpringApplication.run(Main.class,args);
+
     }
 }
